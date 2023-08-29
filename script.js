40cac4525d5f073c30b4ed6dfd2552b45ee13e4f
Haelina <tarany@hotmail.fr>
2023-08-29 14:32:45 -0500

    -Refactoring code -Changed functions to switch cases to define the selected mode and color -Grouped variables, jQuery selectors and event listeners together to improve readability -Rewrote comments

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
-let color;
-let rainbowOn = false;
-let shadowOn = false;
-let backColor;
+//Variables
+let colorSelected;
+let modeSelected;
 
-/* Change variable color value to the value of the color picker
-when color-button is clicked */
-const colorButton = document.getElementById("color-button");
-colorButton.addEventListener('click', () => {color = colorPicker.value; rainbowOn = false});
+//JQuery Selectors
+const inputRange = document.querySelector("#num-of-cells");
+const grid = document.querySelector(".grid-container");
+const clearButton = document.querySelector("#clear-button");
+const colorPicker = document.querySelector("#color-picker");
+const modeButtons = document.querySelectorAll(".mode-choice");
 
-/* Change variable color value depending on the color picker
-Appropriate event is 'input' to listen to the event */
-const colorPicker = document.getElementById("color-picker");
-colorPicker.addEventListener('input', (e) => {color = e.target.value});
+//Event Listeners
+/*Each modeButton is listening for click to define the selected mode
+The color picker button is part of the modeButtons and select the color mode*/
+modeButtons.forEach(modeButton => modeButton.addEventListener('click', changeColor))
+/*The colorPicker gives the variable colorSelected its value
+'input' is the appropriate event for color picker*/
+colorPicker.addEventListener('input', (e) => {colorSelected = e.target.value});
+clearButton.addEventListener('click', clear);
 
+/*Colors the cells depending on the value of the variable modeSelected*/
+function coloringMode() {
+    switch(modeSelected) {
+        case 'rainbow' :
+            this.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+            break;
+        case 'shadow':
 
-let inputRange = document.getElementById("num-of-cells");
+            break;
+        case 'eraser':
+            this.style.backgroundColor = 'white';
+            break;
+        case 'color' :
+            this.style.backgroundColor = colorSelected;
+            break;
+        default :
+            this.style.backgroundColor = 'black';
+            break;
+    }
+}
+
+/*Change the value of the variable modeSelected depending on which button is clicked*/
+function changeColor(e) {
+    switch(e.target.dataset.choice) {
+        case 'rainbow':
+            modeSelected = 'rainbow';
+            break;
+        case 'shadow' :
+            modeSelected = 'shadow';
+            break;
+        case 'eraser' :
+            modeSelected = 'eraser';
+            break;
+        case 'color' :
+            modeSelected = 'color';
+            break;
+        default:
+            modeSelected = color;
+            break;
+    }
+}
+
+/*Clear the grid and change the value of the variable modeSelected
+to the default color mode*/
+function clear() {
+    let squares = document.querySelectorAll(".cell");
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    });
+    modeSelected = "color";
+}
+
 inputRange.addEventListener('input', () => {displayRange(inputRange)});
 
 /* Function called when the slider moves.
@@ -28,7 +83,7 @@ function displayRange(rangeSlider) {
     return sliderVal;
 }
 
-let grid = document.querySelector(".grid-container");
+
 size = displayRange(inputRange);
 
 /*Draw the grid depending on the slider value*/
@@ -41,74 +96,25 @@ function drawGrid(cellsNumber) {
         cell.className = "cell";
         grid.appendChild(cell);
     }
-    changeColor();
-}
-
-
-/*Change the color of the squares when hovering
-depending on the value of the variable constante color and the mode activated*/
-function changeColor() {
     let squares = document.querySelectorAll(".cell");
-    squares.forEach(square => { 
-        square.addEventListener('mouseenter', () => {
-            if (rainbowOn) {
-                square.style.backgroundColor = color; rainbowMode();
-            } else {
-                square.style.backgroundColor = color;
-            }
-        });
-    });
+    squares.forEach(square => square.addEventListener('mouseover', coloringMode))
 }
 
-function selectColor() {
-    if (rainbowOn) {
-        square.style.backgroundColor = color;rainbowMode();
-    } else if (shadowOn) {
-        square.style.backgroundColor = color; shadowMode();
-    }else {
-        square.style.backgroundColor = color;
-    }
-}
 
-/*Clear the grid
-The variable color is reset to the color picker value*/
-const clearButton = document.getElementById("clear-button");
-clearButton.addEventListener('click', () => {clear()});
-function clear() {
-    let squares = document.querySelectorAll(".cell");
-    squares.forEach(square => {
-        square.style.backgroundColor = "white";
-    });
-    if (!rainbowOn && !shadowOn) {
-        color = colorPicker.value;
-    }
-}
 
-/* Change variable color value to white when Eraser button is clicked */
-const eraserButton = document.getElementById("eraser-button");
-eraserButton.addEventListener('click', (e) => {color = "white"});
 
-const rainbowButton = document.getElementById("rainbow-button");
-rainbowButton.addEventListener('click', () => {rainbowMode()});
-function rainbowMode() {
-    rainbowOn = true;
-    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    color = randomColor;
-}
-
-const shadowButton = document.getElementById("shadow-button");
-shadowButton.addEventListener('click', () => {shadowMode()});
-function shadowMode() {
-    shadowOn = true;
-    const r = parseInt(backColor.substr(1, 2), 16)
-    const g = parseInt(backColor.substr(3, 2), 16)
-    const b = parseInt(backColor.substr(5, 2), 16)
-    const a = parseInt(backColor.substr(7, 2), 16)
-    if (a = "") {
-        color = "#" + r + g + b + 0.1;
-    } else {
-        color = "#" + r + g + b + (a+10);
-    }
-} 
+// shadowButton.addEventListener('click', () => {shadowMode()});
+// function shadowMode() {
+//     shadowOn = true;
+//     const r = parseInt(backColor.substr(1, 2), 16)
+//     const g = parseInt(backColor.substr(3, 2), 16)
+//     const b = parseInt(backColor.substr(5, 2), 16)
+//     const a = parseInt(backColor.substr(7, 2), 16)
+//     if (a = "") {
+//         color = "#" + r + g + b + 0.1;
+//     } else {
+//         color = "#" + r + g + b + (a+10);
+//     }
+// } 
 
 
